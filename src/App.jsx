107e300d94a9345cc07f9c9788278a3582665185
dharanichandhar <dharaniv2308@gmail.com>
2025-07-30 03:20:05 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
